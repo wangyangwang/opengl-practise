0c18ac05766bd295611d9ba0d671bb4a0d2f3ea5
YANG WANG <yang.wang.dec@gmail.com>
2018-05-08 20:45:35 -0400

    solid color background

--- glfw opengl/main.cpp
+++ glfw opengl/main.cpp
@@ -1,12 +1,61 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0,0,width,height);
+}
+
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main(){
     glfwInit();
-    
+    //minimum glfw version: 3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //only use the core profile setting of glfw
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "hello OPENGL", nullptr, nullptr);
+    if(window == nullptr){
+        cout << "failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        cout << "failed to initilize GLAD" << endl;
+        return -1;
+    }
+    
+    glViewport(0,0,800,600);
+    //add call back
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        
+        //render
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        //
+        
+        glfwSwapBuffers(window);//output all computed results to screen
+        glfwPollEvents();//hold all received event and execute corresponding functions
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
